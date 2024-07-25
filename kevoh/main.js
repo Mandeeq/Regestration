b1757a9c7f0f5f2a6931670bbe56422b8c7b11dc
itachi <itachi@kali>
2024-07-25 22:40:47 +0300

    created a sample reg page

---
+++ kevoh/main.js
@@ -0,0 +1,16 @@
+function submitForm(){
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+
+    console.log('Username:', username);
+    console.log('Password:', password);
+    console.log('Phone:', phone);
+    console.log('Email:', email);
+
+    
+}
+
+
+
--- regestration.js
+++ regestration.js
@@ -1,9 +1,9 @@
-    document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', (event) => {
     const form = document.querySelector('form');
-    
+
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-        
+
         const firstName = document.querySelector('#first-name').value;
         const lastName = document.querySelector('#last-name').value;
         const email = document.querySelector('#email').value;
@@ -16,16 +16,16 @@
         console.log(`Password: ${password}`);
         console.log(`Number: ${number}`);
 
-       
+
         alert('Form submitted successfully!');
-        
-       
+
+
     });
 
     const loginLink = document.querySelector('.login');
     loginLink.addEventListener('click', (e) => {
         e.preventDefault();
-    
+
         window.location.href = 'Login.html';
     });
 });

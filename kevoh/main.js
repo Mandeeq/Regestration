012c7d5bad39c2fe1e7608f7bf9ec4231422abde
itachi <itachi@kali>
2024-07-27 01:54:17 +0300

    some notes and also added a login page

--- kevoh/main.js
+++ kevoh/main.js
@@ -9,7 +9,26 @@ function submitForm(){
     console.log('Phone:', phone);
     console.log('Email:', email);
 
-    
+    window.location.replace("http://localhost/demos/IMAN/Regestration/kevoh/index.html");
+
+}
+function Login() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById("password").value;
+    console.log("username");
+
+    if (username === "" || password === "") {
+        alert("username or password cannot be blank");
+    } else {
+
+        if (password === "admin" && username === "admin") {
+            alert("succefully logged in");
+            //redirct to homepage in this case im redirecting to a diffrent website just replace the url in 
+            window.location.replace("http://www.w3schools.com");
+        } else {
+            alert("wrong pasword or email");
+        }
+    }
 }
 
 
